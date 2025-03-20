250d63dd6350bb0489fd7e4790ba5fea5117a92e
Aryan Baadlas <154962185+be-a-guptaji@users.noreply.github.com>
2025-03-20 22:43:18 +0530

    Changed code

--- src/actions/lemonSqueezy.ts
+++ src/actions/lemonSqueezy.ts
@@ -4,6 +4,10 @@ export const buySubscription = async (
     buyUserID: string
 ): Promise<ReturnProps> => {
     try {
+        return {
+            status: 200,
+            data: {},
+        }
     } catch (error) {
         return {
             status: 500,
